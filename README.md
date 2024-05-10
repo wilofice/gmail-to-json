19632f25aa688a79cb14ae9125b77121b53adee9
ALAHASSA Généreux <wilofice@gmail.com>
2024-05-10 15:06:30 +0200

    Add scraper.py

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # gmail-to-json
 Export all your Gmail emails to JSON
+
+I personally find Gmail API to be a bit confusing for beginners. The API’s wizard comes handy to create the project, get credentials, and authenticate them. However, the process that should be followed after that is not mentioned clearly.
+
+Therefore, I have created a Python script that does the following:
+1. Scrapes all emails from your mailbox
+2. Exports all emails to JSON file which includes (Datetime, Sender, Subject, Content)
+3. Email content is formatted using bs4
+4. Filter emails by their labels ('IMPORTANT', 'INBOX', 'PROMOTIONS', ...)
+
+
+## Installation
+
+Before anything, make sure to create a project at https://console.developers.google.com/project and 
+create a credential for your app/project. Save/Download the generated credentials.json file in the same directory where you saved 'scraper.py'. If not, set the correct file path in the script here,
+This youtube video shows how to do it: [link]
+
+```python
+flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+```
+
+1. Run the script to install the necessary packages
+```
+bash install.sh
+```
+Then
+
+2. Run scraper.py
+```
+python scraper.py
+```
+
+3. Modify these lines to filter the emails and set a maximum number of emails to download
+```python
+mailFunc.get_msgids_with_labels(label_ids=['IMPORTANT'], max_results = 15)
+```
+
+4. Uncomment these lines from 106 to 114  in 'mailhelpers.py' to download all the emails in your inbox
---
+++ scraper.py
@@ -0,0 +1,53 @@
+import requests
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import base64
+import email
+import json
+from mailhelpers import gmail_func
+import xlsxwriter 
+
+from google.oauth2.credentials import Credentials
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json')
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=10)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('gmail', 'v1', credentials=creds)
+    gmailFunc = gmail_func(service, 'me')
+
+    msg_ids = gmailFunc.get_msgids_with_labels(label_ids=['IMPORTANT'], max_results = 15)
+    msg_id_arr = []
+    for i in msg_ids:
+        msg_id_arr.append(i['id'])
+    gmailFunc.export_json(msg_id_arr)
+    
+if __name__ == '__main__':
+    main()
+
+
+
+
